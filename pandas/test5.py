9d7c3cf865694315c0f2578f27bbcb1431ac6b38
lev2cu <lev2cu@gmail.com>
2014-09-04 09:17:18 -0400

    multiple keys

---
+++ pandas/test5.py
@@ -0,0 +1,11 @@
+
+from pandas import Series, DataFrame
+import pandas as pd
+
+left = DataFrame({'k1': ['sd','we', 'er'], 'k2': ['df','iu','ew'], 'lval': [1,2,3]})
+right = DataFrame({'k1':['rt','sd'], 'k2': ['iu', 'po'], 'rval':[4,5]})
+
+#multiple keys: pass a list of column names
+data = pd.merge(left,right,on=['k1','k2'], how = 'outer')
+
+print data
