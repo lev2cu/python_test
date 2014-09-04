1240fbd95313816088964135bfef6b2d9d2b2450
lev2cu <lev2cu@gmail.com>
2014-09-04 08:41:16 -0400

    outer

--- pandas/test3.py
+++ pandas/test3.py
@@ -1,3 +1,11 @@
-from pandas import Series,DataFrame
+from pandas import Series, DataFrame
 import pandas as pd
 
+df1 = DataFrame({'key': ['b', 'b', 'a', 'c', 'a', 'a', 'b'], 'data1': range(7)})
+df2 = DataFrame({'key': ['a', 'b', 'd'], 'data2': range(3)})
+
+#outer join
+
+data = pd.merge(df1, df2, how = 'outer')
+
+print data
