e3ae83042169720a430ec028e825e0376d7a493a
lev2cu <lev2cu@gmail.com>
2014-09-04 08:51:05 -0400

    inner/left

--- pandas/test4.py
+++ pandas/test4.py
@@ -1,2 +1,18 @@
 from pandas import Series, DataFrame
 import pandas as pd
+
+df1 = DataFrame({'key': ['b', 'b', 'a', 'c'], 'data1': range(4)})
+df2 = DataFrame({'key': ['a', 'b', 'd'], 'data2': range(3)})
+
+#many to many
+
+data = pd.merge(df1, df2, on='key', how = 'outer')
+#
+#inner
+#data = pd.merge(df1, df2, on='key', how = 'inner')
+#left
+#data = pd.merge(df1, df2, on='key', how = 'left')
+#right
+#data = pd.merge(df1, df2, on='key', how = 'right')
+
+print data
